0a7d71cee3068051ef1bbf5c54a99c4a495ca5e9
dy3l <dy3l@users.noreply.github.com>
2021-04-27 22:33:58 +0000

    Modify spider for cosmoselectro.ma

--- scrp/spiders/cosmoselectro.py
+++ scrp/spiders/cosmoselectro.py
@@ -1,10 +1,34 @@
+import datetime
+
 import scrapy
 
 
 class CosmoselectroSpider(scrapy.Spider):
     name = 'cosmoselectro'
     allowed_domains = ['cosmoselectro.ma']
-    start_urls = ['http://cosmoselectro.ma/']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.cosmoselectro.ma', callback=self.parse_urls)
+
+    def parse_urls(self, response):
+        for url in response.css('ul.sub-menu li.level2 a ::attr(href)').extract():
+            yield scrapy.Request(f'{url}?product_list_limit=40', callback=self.parse)
 
     def parse(self, response):
-        pass
+        crawled_at = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+        categories = '|'.join(c.strip() for c in response.css('div.breadcrumbs ul.items li.item ::text').extract() if c.strip())
+        for product in response.css('ol.products li.product'):
+            yield {
+                'title': product.css('a.product-item-link ::text').extract_first().strip(),
+                'brand': None,
+                'model': None,
+                'price': int(product.css('span.price-wrapper ::attr(data-price-amount)').extract_first()) * 100,
+                'categories': categories,
+                'images': product.css('img.product-image-photo ::attr(data-src)').extract_first(),
+                'link': product.css('a.product-item-link ::attr(href)').extract_first().strip(),
+                'available': True,
+                'crawled_at': crawled_at
+            }
+        next_page = response.css('a.next ::attr(href)').extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), self.parse)
